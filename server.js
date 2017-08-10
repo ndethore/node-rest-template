c878017f9d1e569328ebd4fc2ad7c73cc4340c70
Nicolas <nicolas.dethore@gmail.com>
2017-08-10 12:56:33 +0900

    Add dependencies for rest api setup

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log("Hello world!")
+#!/usr/bin/env node
+
+const config  = require('./config');
+const argv    = require('minimist')(process.argv.slice(2));
+const twilio_client  = require('twilio')(config.account.sid, config.account.auth_token);
+const express        = require('express');
+const mongo_client   = require('mongodb').MongoClient;
+
+const app = express();
+const port = 8000;
+
+if (argv.h || argv.help) {
+    console.log([
+        '',
+        '  Usage: server.js [options]',
+        '',
+        '  Options:',
+        '',
+        '    -h, --help    output usage information',
+        '',
+    ].join('\n'));
+    process.exit(0);
+}
+
+app.listen(port, () => {
+    console.log('Listening on port: ' + port);
+});
