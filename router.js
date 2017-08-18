30e8e779ba8fcfd022ac5f8e45d199ba0b2c2ce4
Nicolas <nicolas.dethore@gmail.com>
2017-08-18 19:49:34 +0900

    Add error logging middleware to express app

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ const AuthenticationController  = require('./controllers/authentication');
 const express                   = require('express');
 const passportService           = require('./config/passport');
 const passport                  = require('passport');
+const logger                    = require('morgan');
 
 // Middleware to require login/auth
 const requireAuth = passport.authenticate('jwt', { session: false });
@@ -19,14 +20,37 @@ module.exports = function (app) {
     // Set auth routes as subgroup/middleware to apiRoutes
     apiRoutes.use('/auth', authRoutes);
 
-    // Registration route
+    // Set register route
     authRoutes.post('/register', AuthenticationController.register);
 
-    // Login route
+    // Set login route
     authRoutes.post('/login', requireLogin, AuthenticationController.login);
 
 
 
     // Set url for API group routes
     app.use('/api', apiRoutes);
+
+
+
+    //= ========================
+    // Error Handling
+    //= ========================
+
+    app.use(function (err, req, res, next) {
+      // We log the error internaly
+        logger.error(err);
+
+      //
+      // Remove Error's `stack` property. We don't want
+      // users to see this at the production env
+      //
+        if (req.app.get('env') !== 'development') {
+            delete err.stack;
+        }
+
+      // Finaly respond to the request
+        res.status(err.statusCode || 500).json(err);
+    });
+
 };
