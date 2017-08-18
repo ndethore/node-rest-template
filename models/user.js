f507938f8bc9f21b3ecdd488bff6b5646cc318ce
Nicolas <nicolas.dethore@gmail.com>
2017-08-18 13:17:47 +0900

    Edit user model to use email field instead of username

--- models/user.js
+++ models/user.js
@@ -3,18 +3,16 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const User = new Schema({
-    // email: {
-    //     type: String,
-    //     // lowercase: true,
-    //     // unique: true,
-    //     // required: true
-    // }
-    // password: {
-    //     type: String,
-    //     // required: true
-    // }
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: true
+  },
+  firstName: String,
+  lastName: String
 });
 
-User.plugin(passportLocalMongoose);//, {usernameField: 'email'});
+User.plugin(passportLocalMongoose, {usernameField: 'email', usernameQueryFields: ['email']});
 
 module.exports = mongoose.model('User', User);
