65b6e53d5867321d3e2a179ac0c2569aaffa44fe
Nicolas <nicolas.dethore@gmail.com>
2017-08-18 13:41:19 +0900

    Add email validation before user creation

--- models/user.js
+++ models/user.js
@@ -2,12 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
 const User = new Schema({
   email: {
     type: String,
     lowercase: true,
     unique: true,
-    required: true
+    required: true,
+    validate: [validateEmail, 'Please fill a valid email address']
   },
   firstName: String,
   lastName: String
