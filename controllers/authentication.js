a3765aa61d490c0fb2b43d6914383871a28db05f
Nicolas <nicolas.dethore@gmail.com>
2017-08-18 12:00:36 +0900

    Add support json web token authentication and return token in /register and /login endpoints

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,55 +1,50 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/main');
 var User = require('../models/user');
+
+
+function generateToken(payload) {
+  return jwt.sign(payload, config.secret, { expiresIn: '7d' });
+
+}
 
 exports.register = function(req, res) {
 
     console.log("[+] Registering user " + req.body.username + "...");
     console.log("Body: " + req.body.password);
 
-    User.register(new User({ username : req.body.username }), req.body.password, function(err, account) {
+    const user = new User({ username : req.body.username });
+    User.register(user, req.body.password, function(err, account) {
+
         if (err) {
-
-            // var description;
-
-            // switch (err) {
-            // case MissingUsernameError:
-            //     description = 'You must enter an email address.';
-            // case MissingPasswordError:
-            //     description = 'You must enter a password.';
-            // case UserExistsError:
-            //     description = 'That email address is already in use.';
-            // }
-
             return res.status(422).send({ error: err});
         }
 
         // Subscribe member to Mailchimp list
         // mailchimp.subscribeToNewsletter(user.email);
 
-        // Authenticate user after creation
-        // passport.authenticate('local')(req, res, function () {
-        //     return res.status(201);
-        // });
-
-        // or
-
+        var payload = {
+          '_id': user._id,
+          'username': user.username
+        };
         // Respond with JWT if user was created
-
-        // let userInfo = setUserInfo(user);
-
-        // res.status(201).json({
-        //     token: 'JWT ' + generateToken(userInfo),
-        //     user: userInfo
-        // });
-
-        console.log("No error detected.");
-        return res.status(201).send(req.body.username);
+        return res.status(201).json({
+            token: `${generateToken(payload)}`,
+            user: payload
+        });
 
     });
     console.log("Registration complete.");
 };
 
 exports.login = function(req, res) {
-    //passport.authenticate('local')(req, res, function () {
-    return res.status(200);
-    //});
+    var payload = {
+      '_id': req.user._id,
+      'username': req.user.username
+    };
+
+    return res.status(201).json({
+        token: `${generateToken(payload)}`,
+        user: payload
+    });
 };
